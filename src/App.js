6c2b30d7e885150e6d6cc6f38d25041072bd6591
brijitsaju <brijitsajuthundathil@gmail.com>
2023-12-21 10:57:26 +0530

    interest

---
+++ src/App.js
@@ -0,0 +1,116 @@
+
+import './App.css';
+import { TextField } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import { Button } from '@mui/material';
+import { useState } from 'react';
+
+
+function App() {
+  // principle interest=Principe Amount*Rate Of Interest*Year(yr)/100
+  const [interest, setInterest] = useState(0)
+  const [principle, setPrinciple] = useState(0)
+  const [rate, setRate] = useState(0)
+  const [year, setYear] = useState(0)
+  const [isPrinciple, setIsPrinciple] = useState(true)
+  const [isRate, setIsRate] = useState(true)
+  const [isYear, setIsYear] = useState(true)
+
+  const getValidate = (e) => {
+    const { name, value } = e.target
+    console.log(name, value);
+    if (!!value.match(/^[0-9]*.?[0-9]+$/)) {  //!! to convert into boolean
+      if (name === "principle") {
+        setPrinciple(value)
+        setIsPrinciple(true)
+      }
+      else if (name == "rate") {
+        setRate(value)
+        setIsRate(true)
+      }
+      else if (name == "year") {
+        setYear(value)
+        setIsYear(true)
+      }
+
+    }
+    else {
+      if (name === 'principle') {
+        setPrinciple(value)
+        setIsPrinciple(false)
+      }
+      else if (name === 'rate') {
+        setRate(value)
+        setIsRate(false)
+      }
+      else if (name === 'year') {
+        setYear(value)
+        setIsYear(false)
+      }
+    }
+  }
+
+
+  const handleCalculate = (e) => {
+    e.preventDefault()
+    if (!principle || !rate || !year) {
+      alert('please fill the form')
+    }
+    else {
+      setInterest(principle * rate * year / 100)
+    }
+  }
+
+  const handleReset = (e) => {
+    setInterest(0)
+    setPrinciple(0)
+    setRate(0)
+    setYear(0)
+    setIsPrinciple(true)
+    setIsRate(true)
+    setIsYear(true)
+  }
+  return (
+    <div style={{ height: "100vh" }} className='d-flex justify-content-center align-items-center w-100 bg-dark'>
+      <div className='bg-light p-5 rounded' style={{ width: '500px' }}>
+        <h1>Simple Interest App</h1>
+        <p>calculate simple interest easily</p>
+        <div className='d-flex justify-content-center align-items-center w-100 bg-warning p-3 rounded flex-column'>
+          <h1>₹ {''} {interest}</h1>
+          <p>Total Simple Interest</p>
+        </div>
+        <form className='mt-5' onSubmit={handleCalculate}>
+          <div className='mb-3'>
+            <TextField value={principle || ''} name='principle' onChange={(e) => getValidate(e)} className='w-100 mb-4' id="outlined-basic" label="Principle Amount" variant="outlined" />
+
+            {!isPrinciple &&
+              <div>
+                <p className='text-danger'>Invalid input</p>
+              </div>
+            }
+            <TextField name='rate' value={rate || ''} className='w-100 mb-4' id="outlined-basic" label="Rate Of Interest" variant="outlined" onChange={(e) => getValidate(e)} />
+            {!isRate &&
+              <div>
+                <p className='text-danger'>invalid input</p>
+              </div>
+            }
+            <TextField name='year' value={year || ''} className='w-100 mb-4' id="outlined-basic" label="Year(yr)" variant="outlined" onChange={(e) => getValidate(e)} />
+            {!isYear &&
+              <div>
+                <p className='text-danger'>invalid input</p>
+              </div>
+            }
+
+            <Stack direction="row" spacing={2}>
+              <Button type='submit' disabled={isPrinciple && isRate && isYear ? false : true} className='bg-success' style={{ width: '200px', height: '50px' }} variant="contained">CALCULATE</Button>
+              <Button onClick={handleReset} className='bg-danger text-light ' variant="outlined" style={{ width: '200px', height: '50px' }}>RESET</Button>
+            </Stack>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+
+}
+
+export default App;
